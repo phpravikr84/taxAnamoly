408455ccc89ee23c7821dc516ff6a4b7755c91ca
raviwebadzguru <php.ravikr84@gmail.com>
2024-12-09 11:33:53 +0530

    change urls.py

--- taxAnamoly_main/urls.py
+++ taxAnamoly_main/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.login, name="login"),
     path('login/', views.login, name="login"),
     #
     # path('login/', views.login, name="login"),
